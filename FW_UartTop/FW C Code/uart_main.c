9a72e450465e183494362d8cf66f22cf07e34ed8
kariankk <kariankk@stud.ntnu.no>
2018-06-11 15:56:32 +0200

    coverted tabs to spaces

--- FW_UartTop/FW C Code/uart_main.c
+++ FW_UartTop/FW C Code/uart_main.c
@@ -3,31 +3,31 @@
 
 int main(void){
 
-	//Instantiate test engine manager and inFact sequence
-	infact_sdv_te_mgr te_mgr;
-	infact_uart_RxTx_t *seq;
-	
-	//Create test engine manager with static C code
-	infact_uart_RxTx_static_init(&te_mgr);
-	//Create inFact C code sequence
-	seq = infact_uart_RxTx_create(&te_mgr, "seq");
-	
-	//Initialize UART and TIMER
-	UartRxInit();
-	UartTxInit();
-	TimerInit();
+  //Instantiate test engine manager and inFact sequence
+  infact_sdv_te_mgr te_mgr;
+  infact_uart_RxTx_t *seq;
+  
+  //Create test engine manager with static C code
+  infact_uart_RxTx_static_init(&te_mgr);
+  //Create inFact C code sequence
+  seq = infact_uart_RxTx_create(&te_mgr, "seq");
+  
+  //Initialize UART and TIMER
+  UartRxInit();
+  UartTxInit();
+  TimerInit();
 
-	//Enable UART and start RX
-	StartRx();
+  //Enable UART and start RX
+  StartRx();
 
-	//Run inFact sequence
-	infact_uart_RxTx_run(seq);
+  //Run inFact sequence
+  infact_uart_RxTx_run(seq);
 
-	//Start Tx and disable UART when finished
-	SendTx();
+  //Start Tx and disable UART when finished
+  SendTx();
 
-	//Forever loop to prevent C code from restarting in TB
-	while(true){}
+  //Forever loop to prevent C code from restarting in TB
+  while(true){}
 }
 
 int HardFault_Handler (void){
