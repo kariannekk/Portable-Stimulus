a3e6a2d903e719714af2b06cdc29ff853265b7e9
kariannekk <karianne.kragseth@gmail.com>
2018-06-11 15:52:15 +0200

    removed comment

--- FW_UartTop/FW C Code/product.h
+++ FW_UartTop/FW C Code/product.h
@@ -1,12 +1,4 @@
-/*
- * Copyright (c) 2016 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is confidential property of Nordic
- * Semiconductor. The use, copying, transfer or disclosure of such
- * information is prohibited except by express written agreement with
- * Nordic Semiconductor.
- *
- */
+
 
 #ifndef _PRODUCT_H
 #define _PRODUCT_H
